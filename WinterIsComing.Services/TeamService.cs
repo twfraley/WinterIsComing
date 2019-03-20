227aff7685a948d973e7221991a01e812baae5c6
twfraley <twfraley@gmail.com>
2019-03-20 11:07:51 -0400

    Second Test commit

--- WinterIsComing.Services/TeamService.cs
+++ WinterIsComing.Services/TeamService.cs
@@ -48,12 +48,15 @@ namespace WinterIsComing.Services
                         TeamName = p.TeamName,
                     });
 
+                query.ToList();
+
                 foreach (var item in query)
                 {
+                    item.Characters = GenerateTeamById(item.TeamId);
                     item.TotalPoints = CalculatePoints(item.Characters);
                 }
 
-                return query.ToList();
+                return query;
             }
         }
 
