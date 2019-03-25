fdfbcdb9c1913c7f0c0a611c929d7e8591a81a81
Troy Kuhn <troykuhn22@gmail.com>
2019-03-25 09:50:28 -0400

    redeployment

---
+++ WinterIsComing.Data/Migrations/201903251308559_apidb.cs
@@ -0,0 +1,175 @@
+namespace WinterIsComing.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class apidb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Character",
+                c => new
+                    {
+                        CharacterId = c.Int(nullable: false, identity: true),
+                        CharacterName = c.String(nullable: false),
+                        House = c.String(nullable: false),
+                        ImageLink = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.CharacterId);
+            
+            CreateTable(
+                "dbo.PointValue",
+                c => new
+                    {
+                        PointValueId = c.Int(nullable: false, identity: true),
+                        CharacterId = c.Int(nullable: false),
+                        SurvivedEpisode = c.Boolean(nullable: false),
+                        EpisodeAppearance = c.Boolean(nullable: false),
+                        GetKill = c.Boolean(nullable: false),
+                        Death = c.Boolean(nullable: false),
+                        BigKill = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.PointValueId)
+                .ForeignKey("dbo.Character", t => t.CharacterId, cascadeDelete: true)
+                .Index(t => t.CharacterId);
+            
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.TeamCharacter",
+                c => new
+                    {
+                        TeamCharacterId = c.Int(nullable: false, identity: true),
+                        UserId = c.Guid(nullable: false),
+                        CharacterId = c.Int(nullable: false),
+                        TeamId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TeamCharacterId)
+                .ForeignKey("dbo.Character", t => t.CharacterId, cascadeDelete: true)
+                .ForeignKey("dbo.Team", t => t.TeamId, cascadeDelete: true)
+                .Index(t => t.CharacterId)
+                .Index(t => t.TeamId);
+            
+            CreateTable(
+                "dbo.Team",
+                c => new
+                    {
+                        TeamId = c.Int(nullable: false, identity: true),
+                        TeamName = c.String(nullable: false),
+                        UserId = c.Guid(nullable: false),
+                        TotalPoints = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TeamId);
+            
+            CreateTable(
+                "dbo.Theory",
+                c => new
+                    {
+                        TheoryId = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
+                        Subject = c.String(nullable: false),
+                        Content = c.String(nullable: false),
+                        DateCreated = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.TheoryId);
+            
+            CreateTable(
+                "dbo.ApplicationUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.TeamCharacter", "TeamId", "dbo.Team");
+            DropForeignKey("dbo.TeamCharacter", "CharacterId", "dbo.Character");
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.PointValue", "CharacterId", "dbo.Character");
+            DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.TeamCharacter", new[] { "TeamId" });
+            DropIndex("dbo.TeamCharacter", new[] { "CharacterId" });
+            DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.PointValue", new[] { "CharacterId" });
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.ApplicationUser");
+            DropTable("dbo.Theory");
+            DropTable("dbo.Team");
+            DropTable("dbo.TeamCharacter");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityRole");
+            DropTable("dbo.PointValue");
+            DropTable("dbo.Character");
+        }
+    }
+}
--- WinterIsComing.WebApi/App_Start/WebApiConfig.cs
+++ WinterIsComing.WebApi/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace WinterIsComing.WebApi
     {
         public static void Register(HttpConfiguration config)
         {
-            var enableCorsAttribute = new EnableCorsAttribute("http://localhost:4200", "*", "*");
+            var enableCorsAttribute = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(enableCorsAttribute);
 
             // Web API configuration and services
